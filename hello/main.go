cb3c644a0e8f38f276e67fe15227a63fbfa80fcc
Enze Chi <Enze.Chi@gmail.com>
2023-07-17 21:09:18 +1000

    Add create a go module example

--- hello/main.go
+++ hello/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
-	"rsc.io/quote"
+
+	"example.com/greetings"
 )
 
 func main() {
-	fmt.Println(quote.Go())
+	// Get a greeting message and print it.
+	message := greetings.Hello("Gladys")
+	fmt.Println(message)
 }
